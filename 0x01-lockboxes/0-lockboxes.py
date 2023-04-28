b7863dce1acd2c94576df1557a59506ecbcf60d3
BlightG <birhane.gulilat@gmail.com>
2023-04-28 11:38:04 +0300

    added guards to ensure only proper keys are enterd to the keyset

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,26 +4,36 @@
 
 def canUnlockAll(boxes):
     """ can unlock boxes """
+    # return true if the box has only one portion at index 0
     if len(boxes) == 1:
         return True
 
+    # initialize an empty set to store the keys
     keyset = set()
 
+    # add keys found in the index 0 the unlocked box to keyset
     for i in boxes[0]:
-        keyset.add(i)
+        if i < len(boxes):
+            keyset.add(i)
 
+    
     lenght = 0
+    # checks if the amount of k in the boxes is less than the amount of boxes
     while lenght < len(keyset):
+        # set length as lenght of keyset
         lenght = len(keyset)
+        # loop over the copy of the set 
         for i in keyset.copy():
-            for j in boxes[i]:
-                if j < len(boxes):
-                    keyset.add(j)
+            # add keys to keyset that have mathicing boxes
+            if i < len(boxes):
+                for j in boxes[i]:
+                    if j < len(boxes):
+                        keyset.add(j)
 
+    # remove 0 if in the set since index 0 is always unlocked
     if 0 in keyset:
         keyset.remove(0)
 
-    # print(f'keyset > {len(keyset)} boxes > {len(boxes)}')
     if len(keyset) + 1 == len(boxes):
         return True
 
