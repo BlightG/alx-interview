5b60f67205df1cc17d6d9ca3c2c4861463202880
BlightG <birhane.gulilat@gmail.com>
2023-05-07 15:23:35 +0300

    added a new file

---
+++ 0x00-pascal_triangle/0-pascal_triangl.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Pascal Triangle Interview Challenge """
+
+
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to n rows.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+
+    Raises:
+        ValueError: If n is negative.
+    """
+   if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
