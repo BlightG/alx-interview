959bf45494d308b3f0b6f1b1bd6da0b8293c0552
BlightG <birhane.gulilat@gmail.com>
2023-05-17 14:07:43 +0300

    task 4 first attempt

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,3 +3,16 @@
 
 
 def validUTF8(data) -> bool:
+    """ a function to validate utf-8 """
+
+    if data is None:
+        return False
+
+    if None in data:
+        return False
+
+    for i in data:
+        if i >= 256:
+            return False
+
+    return True
