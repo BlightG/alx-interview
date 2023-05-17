759e9459ab74fcd5e3a496ecea55b98b9fca29fe
BlightG <birhane.gulilat@gmail.com>
2023-05-17 14:31:38 +0300

    task 4 modifcation of first trial range

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,17 @@ def validUTF8(data) -> bool:
         return False
 
     for i in data:
-        if i >= 256:
+        if i < 128:
+            continue
+        else if i >= 128 and i <= 256:
             return False
 
+        # bin_list = bin(i)[2:].zfill()
+        # print(f'bin_list = {bin_list} type = {type(bin_list)} for i = {i} ')
+
     return True
+
+# def bin_list(integer: int) -> list:
+#     """ converts an int to a binary """
+
+    
