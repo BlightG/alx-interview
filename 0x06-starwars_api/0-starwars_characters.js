3ed71892d0c1f73671064283c19a9b3e1f29af62
BlightG <birhane.gulilat@gmail.com>
2023-06-01 18:53:49 +0300

    new implementation using promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,33 @@
 #!/usr/bin/node
 const request = require('request');
+const { promisify } = require('util');
 
 const film = process.argv[2];
+const promisifiedReq = promisify(request);
 
-if (film >= 1 && film <= 7) {
-  request('https://swapi-api.alx-tools.com/api/films/' + film, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const reply = JSON.parse(body);
-      reply.characters.forEach(element => {
-        request(element, (error, response, body) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const Creply = JSON.parse(body);
-            console.log(Creply.name);
-          }
-        });
-      });
+// (async () => {
+//   if (film >= 1 && film <= 7) {
+//     const res = await promisifiedReq(`https://swapi-api.alx-tools.com/api/films/${film}`)
+//     const reply = JSON.parse(res.body)
+//     for (let characterLink of reply.characters) {
+//       const currChar = await promisifiedReq(characterLink);
+//       const charRes = JSON.parse(currChar.body)
+//       console.log(charRes.name)
+//     }
+//   }
+// })()
+(async () => {
+  if (film >= 1 && film <= 7) {
+    const res = await promisifiedReq(`https://swapi-api.alx-tools.com/api/films/${film}`);
+    const reply = JSON.parse(res.body);
+    const allRes = [];
+    for (const characterLink of reply.characters) {
+      allRes.push(promisifiedReq(characterLink));
     }
-  });
-}
+    const all = await Promise.all(allRes);
+    for (const item of all) {
+      const parsedItem = JSON.parse(item.body);
+      console.log(parsedItem.name);
+    }
+  }
+})();
